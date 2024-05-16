bed95ab06af50bd21fd0ebe03b63bebf7afedec0
arjun1127 <arjunkrao2004@gmail.com>
2024-05-16 06:38:29 +0530

    getting face hand landmarks using mediapipe holistic functions , extracting key-points and drawing landmarks

--- Main.py
+++ Main.py
@@ -20,6 +20,25 @@ def mediapipe_detection(image, model):
     return image, results
 
 
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(254, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
 # to capture the video by frames we have set up a loop
 
 cap = cv2.VideoCapture(0)
@@ -30,8 +49,9 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         # make detection
         image, results = mediapipe_detection(frame, holistic)
         print(results)
+        draw_landmarks(image, results)
         # to show the popup window and the img keeps on processing till while is true
-        cv2.imshow('OpenCv Feed', frame)
+        cv2.imshow('OpenCv Feed', image)
         # quit the pop-up as u press 'q'
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
@@ -39,5 +59,28 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 cap.release()
 cv2.destroyAllWindows()
 
-# see results using results.face_landmark.Landmark or and landmarks left hand or right hand
 
+# see results using results.face_landmark.Landmark or and landmarks left hand or right hand
+# results.landmarks for each face, pose, hands has its set of parameters x,y,z
+
+# print(len(results.pose_landmarks.landmark)*4)
+# print(len(results.face_landmarks.landmark)*3)
+# print(len(results.left_hand_landmarks.landmark)*3)
+# print(len(results.right_hand_landmarks.landmark)*3)
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+    lh = np.array([[res.x, res.y, res.z] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 3)
+    # for slr concatenate all the keypoints
+    return np.concatenate([pose, face, lh, rh])
+
+
+# print(rh)
+# print(extract_keypoints(results).shape) => (1662,)
